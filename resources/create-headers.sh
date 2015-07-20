5f2c232cddbf4d2cd7d8edf3a827ae5d95fa8e00
Duncan Marsh <dwolfm@gmail.com>
2015-07-20 16:28:37 -0700

    if im gunna learn c gota just write stuff in c :),,, started making the build tools in c "

--- resources/create-headers.sh
+++ resources/create-headers.sh
@@ -4,12 +4,8 @@ cd head
 printline () {
   filepath=$1
   linecout=$2
-  #echo $1 $2 $3
   tailnum=$(expr $2 - $3 + 1)
-  #tailnum=$(expr $2 + 1 + $3)
-  #echo $tailnum
   tail -n $tailnum $filepath| head -n 1
-  #echo "lulwat $tailnum"
 }
 
 for dir in `ls`;do
@@ -18,24 +14,9 @@ for dir in `ls`;do
       if [ -f $PWD/$dir/$file ]; then 
         thefile=$PWD/$dir/$file 
         numlines=`wc -l $thefile |cut -d' ' -f 1`
-        #cat $thefile
-        #echo $thefile
-        
         for line in `seq 1 $numlines`; do
-          #linedata=`sed -n "$line,$line p" $thefile`
-          lul=printline $thefile $nums $line
-
-          echo $lul >> ../lulwat
-
-
-          #echo $ha
-          ##doublebackslash=$(echo -E $linedata | sed 's.\\.\\\\.g')
-          #help=$(echo -e"       hah")
-          #echo $linedata
-          #echo $help >> ../lul.wat
-          
-          #echo "cafy $cafy"  
-          #echo -E " $line of $thefile $linedata" 
+          printline $thefile $nums $line | sed 's.\\.\\\\.g'
+          echo $ha
         done
       fi
     done  
---
+++ tools/include/setup.h
@@ -0,0 +1,6 @@
+#ifndef _setup_h
+#define _setup_h
+
+#define PROJ_PATH "/home/nacnud/gitz/talkinghead"
+
+#endif
